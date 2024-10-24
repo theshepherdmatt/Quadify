c216511fe7a5b42d759e138e2aa72d3e263e7132
Quadify <matt.theshepherd@gmail.com>
2024-10-24 15:40:24 +0100

    Update Workaround_BuildEssentials.sh

--- oled/Workaround_BuildEssentials.sh
+++ oled/Workaround_BuildEssentials.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
+set -e
 
 # Define the APT sources list file
 SRC=/etc/apt/sources.list
 
 # Backup the original sources list in case something goes wrong
-cp $SRC $SRC.backup
+sudo cp "$SRC" "${SRC}.backup"
 
 # Check if 'buster' is not already in the sources list
-if ! grep -q 'buster' $SRC; then
-    # Add the Debian Buster repositories for package installation
+if ! grep -q 'buster' "$SRC"; then
     echo "Adding Buster sources..."
-    echo 'deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi' | sudo tee -a $SRC
+    echo 'deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi' | sudo tee -a "$SRC"
 fi
 
 # Update the package lists
-apt update
+sudo apt-get update
 
 # Install the required packages
-apt -y install binutils libstdc++-6-dev gcc-8 gcc g++-8 g++
+sudo apt-get install -y binutils libstdc++-6-dev gcc-8 g++-8 build-essential python3
+
+# Set gcc-8 and g++-8 as the default compilers
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 100
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 100
+
+# Verify the default gcc and g++ versions
+gcc --version
+g++ --version
 
 # Remove the Buster sources to prevent future compatibility issues
-sed -i '/buster/d' $SRC
+sudo sed -i '/buster/d' "$SRC"
 
 # Update the package lists after removing the Buster sources
-apt update
+sudo apt-get update
 
 echo "Installation complete. Buster sources removed."
-
